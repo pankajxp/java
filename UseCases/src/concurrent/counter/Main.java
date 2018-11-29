85145ae843b18e0192ad942d99fba0d82b906968
Pankaj Agnihotri <panagnih@publicisgroupe.net>
2018-11-29 18:46:10 +0530

    removed unwanted files.

--- UseCases/src/concurrent/counter/Main.java
+++ UseCases/src/concurrent/counter/Main.java
@@ -1,48 +1,95 @@
 package concurrent.counter;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
-	
-	volatile static Integer intCounter;
 
-	public static void main(String[] args) throws InterruptedException {
-		ConcurrentCounter counter = new ConcurrentCounter(0);
-		
-		intCounter = new Integer(0);
+	static volatile AtomicInteger sharedCounter;
 
-		Thread t1 = new CounterThread(counter, intCounter);
-		Thread t2 = new CounterThread(counter, intCounter);
-		Thread t3 = new CounterThread(counter, intCounter);
-		Thread t4 = new CounterThread(counter, intCounter);
-		
-		t1.start();t2.start();t3.start();t4.start();
-		
-		t1.join();t2.join();t3.join();t4.join();
+	static AtomicBoolean isShutdown = new AtomicBoolean(false);
 
-		System.out.println("final concurrent counter value: " + counter.getcount());
-		System.out.println("final integer counter value: " + intCounter.toString());
+	static volatile AtomicInteger threadCounter;
+
+	final static int MAX_THREAD_COUNT = 4;
+
+	final static int MAX_COUNTER = 10;
+
+	public final static CyclicBarrier cb = new CyclicBarrier(MAX_THREAD_COUNT+1);
+
+	volatile List<Integer> threadSequence = new LinkedList<>();
+
+	public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
+//		ConcurrentCounter counter = new ConcurrentCounter(0);
+
+		sharedCounter = new AtomicInteger(1);
+		threadCounter = new AtomicInteger(1);
+
+		Thread t1 = new CounterThread(1, sharedCounter);
+		Thread t2 = new CounterThread(2, sharedCounter);
+		Thread t3 = new CounterThread(3, sharedCounter);
+		Thread t4 = new CounterThread(4, sharedCounter);
+
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+
+		cb.await();
+
+		System.out.println("final concurrent counter value: " + sharedCounter.get());
+//		System.out.println("final integer counter value: " + sharedCounter.toString());
 	}
 
 }
 
 class CounterThread extends Thread {
 	private boolean isConcurrentCounter = true;
-	private ConcurrentCounter counter;
-	private static Integer intCounter;
+	private AtomicInteger counter;
+	private Integer name;
 
-	public CounterThread(ConcurrentCounter counter, Integer intCounter) {
+	public CounterThread(Integer name, AtomicInteger counter) {
 		super();
 		this.counter = counter;
-		this.intCounter = intCounter;
+		this.name = name;
 	}
 
 	public void run() {
-		if(isConcurrentCounter){
-			for (int i = 0; i < 10; i++) {
-				System.out.println(Thread.currentThread() + " increments : " + counter.increment());
+		while (true) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} else {
-			for (int i = 0; i < 10; i++) {
-				System.out.println(Thread.currentThread() + " increments : " + intCounter++);
+			if (Main.threadCounter.get() == name) {
+				if (Main.MAX_THREAD_COUNT == name) {
+					Main.threadCounter.compareAndSet(Main.MAX_THREAD_COUNT, 0);
+				}
+				Main.threadCounter.incrementAndGet();
+				System.out.println("Thread name: " + name + ", Counter: " + counter.getAndIncrement());
+				if (counter.get() == Main.MAX_COUNTER) {
+					Main.isShutdown.set(true);
+					try {
+						Main.cb.await();
+					} catch (InterruptedException | BrokenBarrierException e) {
+						e.printStackTrace();
+					}
+					return;
+				}
+
+			} else {
+				if (Main.isShutdown.get() == true) {
+					try {
+						Main.cb.await();
+					} catch (InterruptedException | BrokenBarrierException e) {
+						e.printStackTrace();
+					}
+					return;
+				}
 			}
 		}
 	}
--- UseCases/src/concurrent/hashmap/lock/splitting/MyConcurrentHashMap.java
+++ UseCases/src/concurrent/hashmap/lock/splitting/MyConcurrentHashMap.java
@@ -4,92 +4,94 @@ import java.util.HashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class MyConcurrentHashMap<K,V> {
-	
+public class MyConcurrentHashMap<K, V> {
+
 	private static int cuncurrencyLevel = 16;
-	
+
 	/**
-     * The segments, each of which is a hash map.
-     */
-    private MySegment<K,V>[] segments;
-	
-    static final int MIN_SEGMENT_SIZE = 2;
-    
-    public class MySegment<K, V> extends HashMap<K, V> {
+	 * The segments, each of which is a hash map.
+	 */
+	private MySegment<K, V>[] segments;
+
+	static final int MIN_SEGMENT_SIZE = 2;
+
+	public class MySegment<K, V> extends HashMap<K, V> {
 		private static final long serialVersionUID = 1L;
 
 		public MySegment(int capacity, float lf) {
-    		super(capacity, lf);
-    	}
+			super(capacity, lf);
+		}
 
-    	private Lock lock = new ReentrantLock();
-    	
-    	public V put (K key, V value) {
-    		lock.lock();
-    		try{
-    		return super.put(key, value);
-    		} finally {
-    			lock.unlock();
-    		}
-    	}
-    	
-    	public V get(Object key) {
-    		return super.get(key);
-    	}
-    	
-    }
-    
-    public MyConcurrentHashMap(final int initialCapacity) {
-    	this(initialCapacity,0.75f, cuncurrencyLevel);
-    }
-    public MyConcurrentHashMap(final int initialCapacity, final float loadFactor, final int concurrencyLevel) {
-    	cuncurrencyLevel = concurrencyLevel;
-    	segments = new MySegment[cuncurrencyLevel];
-    	//size of each hashmap
-    	int cap = initialCapacity/concurrencyLevel;
-    	if(cap < MIN_SEGMENT_SIZE) {
-    		cap = MIN_SEGMENT_SIZE;
-    	} for (int i = 0 ; i< concurrencyLevel; i++) {
-    		segments[i] = new MySegment<>(cap, loadFactor);
-    	}
-    }
-    
+		private Lock lock = new ReentrantLock();
+
+		public V put(K key, V value) {
+			lock.lock();
+			try {
+				return super.put(key, value);
+			} finally {
+				lock.unlock();
+			}
+		}
+
+		public V get(Object key) {
+			return super.get(key);
+		}
+
+	}
+
+	public MyConcurrentHashMap(final int initialCapacity) {
+		this(initialCapacity, 0.75f, cuncurrencyLevel);
+	}
+
+	public MyConcurrentHashMap(final int initialCapacity, final float loadFactor, final int concurrencyLevel) {
+		cuncurrencyLevel = concurrencyLevel;
+		segments = new MySegment[cuncurrencyLevel];
+		// size of each hashmap
+		int cap = initialCapacity / concurrencyLevel;
+		if (cap < MIN_SEGMENT_SIZE) {
+			cap = MIN_SEGMENT_SIZE;
+		}
+		for (int i = 0; i < concurrencyLevel; i++) {
+			segments[i] = new MySegment<>(cap, loadFactor);
+		}
+	}
+
 	private transient final int hashSeed = 0;
-	
+
 	private int hash(Object k) {
-        int h = hashSeed;
+		int h = hashSeed;
 
-        h ^= k.hashCode();
+		h ^= k.hashCode();
 
-        // Spread bits to regularize both segment and index locations,
-        // using variant of single-word Wang/Jenkins hash.
-        return (h ^ (h >>> 16)) % cuncurrencyLevel;
-    }
-	
+		// Spread bits to regularize both segment and index locations,
+		// using variant of single-word Wang/Jenkins hash.
+		return (h ^ (h >>> 16)) % cuncurrencyLevel;
+	}
+
 	public V put(K key, V value) {
 		MySegment<K, V> s;
-		if(value == null) {
+		if (value == null) {
 			throw new NullPointerException();
 		}
 		int index = hash(key);
-		
+
 		s = segments[index];
-		
+
 		return s.put(key, value);
 	}
-	
+
 	public V get(K key) {
 		MySegment<K, V> s;
 		int index = hash(key);
 		s = segments[index];
 		return s.get(key);
 	}
-	
-	public static void main(String [] args) {
-		MyConcurrentHashMap<String, String> myChm = new MyConcurrentHashMap<>(16);
-		myChm.put("hello", "john");
-		myChm.put("hi", "kapil");
-		System.out.println(myChm.get("hello"));
-		System.out.println(myChm.get("hi"));
+
+	public static void main(String[] args) {
+		MyConcurrentHashMap<Integer, String> myChm = new MyConcurrentHashMap<>(16);
+		myChm.put(1, "john");
+		myChm.put(2, "kapil");
+		System.out.println("value for key 1 :" + myChm.get(1));
+		System.out.println("value for key 2 :" + myChm.get(2));
 	}
 }
